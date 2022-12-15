0dc99521e54348fe19402642cd1ec41ff879c06b
Tayanne <tayh.snovaes@gmail.com>
2022-12-15 19:40:02 -0300

    feat: implement second step

---
+++ src/application/entities/notification.ts
@@ -0,0 +1,64 @@
+import { Replace } from 'src/helpers/Replace';
+import { Content } from './content';
+import { randomUUID } from 'node:crypto';
+
+export interface NotificationProps {
+  recipientId: string;
+  content: Content;
+  category: string;
+  readAt?: Date | null;
+  createdAt: Date;
+}
+
+export class Notification {
+  private _id: string;
+  private props: NotificationProps;
+
+  constructor(props: Replace<NotificationProps, { createdAt?: Date }>) {
+    this._id = randomUUID();
+    this.props = {
+      ...props,
+      createdAt: props.createdAt ?? new Date(),
+    };
+  }
+
+  get id(): string {
+    return this._id;
+  }
+
+  set recipientId(recipientId: string) {
+    this.props.recipientId = recipientId;
+  }
+
+  get recipientId(): string {
+    return this.props.recipientId;
+  }
+
+  set content(content: Content) {
+    this.props.content = content;
+  }
+
+  get content(): Content {
+    return this.props.content;
+  }
+
+  set category(category: string) {
+    this.props.category = category;
+  }
+
+  get category(): string {
+    return this.props.category;
+  }
+
+  set readAt(readAt: Date | null | undefined) {
+    this.props.readAt = readAt;
+  }
+
+  get readAt(): Date | null | undefined {
+    return this.props.readAt;
+  }
+
+  get createdAt(): Date {
+    return this.props.createdAt;
+  }
+}
